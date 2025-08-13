a25a8c90d1e5a7cd454a18975614694ead0f20f9
kaiyikang <kaiyikang@gmail.com>
2025-08-13 20:38:27 +0200

    Enhance InitParameters and AnnoUtils to support filter initialization and parameter management

--- src/main/java/com/kaiyikang/minitomcat/engine/support/InitParameters.java
+++ src/main/java/com/kaiyikang/minitomcat/engine/support/InitParameters.java
@@ -1,5 +1,56 @@
 package com.kaiyikang.minitomcat.engine.support;
 
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class InitParameters extends LazyMap<String> {
+
+    public boolean setInitParameter(String name, String value) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("Name is null or empty.");
+        }
+        if (value.isBlank()) {
+            throw new IllegalArgumentException("Value is null or empty.");
+        }
+        if (super.containsKey(name)) {
+            return false;
+        }
+        super.put(name, value);
+        return true;
+    }
+
+    public Set<String> setInitParameters(Map<String, String> initParameters) {
+        if (initParameters == null) {
+            throw new IllegalArgumentException("initParameters is null.");
+        }
+        if (initParameters.isEmpty()) {
+            return Set.of();
+        }
+
+        Set<String> conflicts = new HashSet<>();
+        for (String name : initParameters.keySet()) {
+            String value = initParameters.get(name);
+            if (value == null) {
+                throw new IllegalArgumentException("initParameters contains null value by name: " + name);
+            }
+            if (super.containsKey(name)) {
+                conflicts.add(name);
+            } else {
+                super.put(name, value);
+            }
+        }
+        return conflicts;
+    }
+
+    public Map<String, String> getInitParameters() {
+        return super.map();
+    }
+
+    public Enumeration<String> getInitParameterNames() {
+        return Collections.enumeration(super.map().keySet());
+    }
 
 }
--- src/main/java/com/kaiyikang/minitomcat/utils/AnnoUtils.java
+++ src/main/java/com/kaiyikang/minitomcat/utils/AnnoUtils.java
@@ -1,13 +1,16 @@
 package com.kaiyikang.minitomcat.utils;
 
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.EnumSet;
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.Filter;
 import jakarta.servlet.Servlet;
+import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.annotation.WebInitParam;
 import jakarta.servlet.annotation.WebServlet;
 
@@ -20,8 +23,24 @@ public class AnnoUtils {
         return defaultNameByClass(servletClass);
     }
 
+    public static String getFilterName(Class<? extends Filter> filterClass) {
+        WebFilter w = filterClass.getAnnotation(WebFilter.class);
+        if (w != null && !w.filterName().isEmpty()) {
+            return w.filterName();
+        }
+        return defaultNameByClass(filterClass);
+    }
+
     public static Map<String, String> getServletInitParams(Class<? extends Servlet> servletClass) {
         WebServlet w = servletClass.getAnnotation(WebServlet.class);
+        if (w == null) {
+            return Map.of();
+        }
+        return initParamsToMap(w.initParams());
+    }
+
+    public static Map<String, String> getFilterInitParams(Class<? extends Filter> filterClass) {
+        WebFilter w = filterClass.getAnnotation(WebFilter.class);
         if (w == null) {
             return Map.of();
         }
@@ -36,7 +55,17 @@ public class AnnoUtils {
         return arraysToSet(w.value(), w.urlPatterns()).toArray(String[]::new);
     }
 
-    private static String defaultNameByClass(Class<? extends Servlet> servletClass) {
+    public static EnumSet<DispatcherType> getFilDispatcherTypes(Class<? extends Filter> filterClass) {
+        WebFilter w = filterClass.getAnnotation(WebFilter.class);
+        if (w == null) {
+            return EnumSet.of(DispatcherType.REQUEST);
+        }
+        return EnumSet.copyOf(Arrays.asList(w.dispatcherTypes()));
+    }
+
+    // Private methods
+
+    private static String defaultNameByClass(Class<?> servletClass) {
         String name = servletClass.getSimpleName();
         name = Character.toLowerCase(name.charAt(0)) + name.substring(1);
         return name;
