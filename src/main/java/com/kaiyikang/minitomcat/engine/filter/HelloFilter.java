0e02657182b1e2f7c3a1e4674ad07c8f4308da3f
ValMobYKang <yikai.kang@valtech.com>
2025-08-13 08:10:06 +0200

    Add HelloFilter to restrict access based on name parameter

---
+++ src/main/java/com/kaiyikang/minitomcat/engine/filter/HelloFilter.java
@@ -0,0 +1,40 @@
+package com.kaiyikang.minitomcat.engine.filter;
+
+import java.io.IOException;
+import java.net.http.HttpRequest;
+import java.util.Set;
+
+import org.slf4j.Logger;
+
+import org.slf4j.LoggerFactory;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebFilter(urlPatterns = "/hello")
+public class HelloFilter implements Filter {
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    Set<String> names = Set.of("Bob", "Alice", "Tom", "Jerry");
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        String name = req.getParameter("name");
+        if (name != null && names.contains(name)) {
+            chain.doFilter(request, response);
+        } else {
+            logger.warn("Access denied: name = {}", name);
+            HttpServletResponse resp = (HttpServletResponse) response;
+            resp.sendError(403, "Forbidden");
+        }
+    }
+
+}
